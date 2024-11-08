344ce8be777fd9124a8071bcbf2a03a2aac77137
shunicle <privetgygy35@gmail.com>
2024-11-08 14:51:54 +0300

    Добавьте файлы проекта.

---
+++ Лаб 1 упр 2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаб_1_упр_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddPolygon(new Point[] { new Point(0, 0), new Point(0, this.Height), new Point(0, this.Height) });
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+        }
+    }
+}
